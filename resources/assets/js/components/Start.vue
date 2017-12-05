4e0d85837b6f86ddb16f8e2df0c65720c37a8990
Angelica-dahurica <151250132@smail.nju.edu.cn>
2017-12-05 09:51:05 +0800

    开始界面的整改

--- resources/assets/js/components/Start.vue
+++ resources/assets/js/components/Start.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="home-page">
-        <div class="main-page">
-            <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
-                <router-link to="/homepage"><el-menu-item index="1">首页</el-menu-item></router-link>
-                <router-link to="/find"><el-menu-item index="2">发现</el-menu-item></router-link>
-                <router-link to="/choice"><el-menu-item index="3">精选</el-menu-item></router-link>
-            </el-menu>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+            <router-link to="/homepage"><el-menu-item index="1">首页</el-menu-item></router-link>
+            <router-link to="/find"><el-menu-item index="2">发现</el-menu-item></router-link>
+            <router-link to="/choice"><el-menu-item index="3">精选</el-menu-item></router-link>
+        </el-menu>
+        <div class="main-page" v-if="tag">
             <div class="sign">
                 <router-link to="/signin" class="home-sign-in">登录</router-link>
                 <router-link to="/signup" class="home-sign-up">注册</router-link>
@@ -13,10 +13,12 @@
             <div class="title">petrichor</div>
             <div class="description">the earthy scent produced when rain falls on dry soil</div>
             <div class="description">a website for social</div>
-            <router-link to="/homepage/find">
-                <el-input icon="search" v-model="input" :on-icon-click="handleIconClick">
-                </el-input>
-            </router-link>
+            <el-input icon="search" v-model="input" :on-icon-click="handleIconClick"></el-input>
+        </div>
+        <div class="picture-list" v-else>
+            <div class="picture" v-model="pictures" v-for="picture in pictures">
+                <img height="280px" :src=picture.picture_content>
+            </div>
         </div>
     </div>
 </template>
@@ -25,6 +27,7 @@
     export default {
         data () {
             return {
+                tag: true,
                 input: '',
                 pictures: [],
                 activeIndex: '1',
@@ -32,11 +35,15 @@
         },
         methods: {
             handleIconClick (ev) {
-                const url = this.$store.state.server + 'search/?query=' + this.input;
-                this.$http.get(url).then((response) => {
+                this.$http.get('/pictures/description=' + this.input).then((response) => {
                     response = response.body;
-                    this.pictures = response.subjects
-                })
+                    this.pictures = response.data
+                });
+                this.tag = false;
+            },
+            handleSelect(key, keyPath) {
+                this.tag = true;
+                this.input = '';
             }
         }
     }
@@ -45,11 +52,13 @@
 <style lang="stylus" rel="stylesheet/stylus">
     .home-page
         margin 0
+        .el-menu
+            background-color #F4F8DF
         .main-page
             margin 0
-            height 599px
             text-align center
             font-family "Buxton Sketch"
+            height 599px
             background url("/images/home.jpg")
             .sign
                 width 98%
@@ -71,4 +80,15 @@
             .el-input
                 width 280px
                 margin-top 20px
+        .picture-list
+            background-image none!important
+            padding 30px 20px
+            .picture
+                display inline-block
+                margin 10px 10px
+                height 280px
+                border-radius 5px
+                overflow hidden
+                .img
+                    margin 0
 </style>
